7ca57c53e005793037b7306df8df04e52ae2106e
Mateusz Żebrak <mateusz.zebrak@gmail.com>
2022-03-13 00:21:12 +0100

    Add CORS middleware

--- steam_deals/v1/api.py
+++ steam_deals/v1/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from steam_deals.config import settings, VERSION
 from steam_deals.v1.routers import main_router
@@ -16,5 +17,14 @@ app = FastAPI(
         'url': 'https://opensource.org/licenses/MIT',
     },
 )
+
+if settings.get('ALLOW_ORIGINS', None):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.ALLOW_ORIGINS,
+        allow_credentials=settings.get('ALLOW_CREDENTIALS', True),
+        allow_methods=settings.get('ALLOW_METHODS', ['*']),
+        allow_headers=settings.get('ALLOW_HEADERS', ['*']),
+    )
 
 app.include_router(main_router)
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,8 @@ from fastapi import status
 
 import git
 
+from steam_deals.config import settings
+
 
 def test_connection(api_client):
     # ACT
@@ -23,3 +25,20 @@ def test_index(api_client, index_response):
     # ASSERT
     assert response.status_code == status.HTTP_200_OK
     assert result in [index_response(version=sha), index_response(version='UNCOMMITTED')]
+
+
+def test_cors_header(api_client):
+    # ARRANGE
+    valid_origins = settings.ALLOW_ORIGINS
+    invalid_origins = ['http://localhost:3201', 'http://localhost:4001']
+
+    # ACT
+    valid_responses = [api_client.get('/', headers={'Origin': origin}) for origin in valid_origins]
+    invalid_responses = [api_client.get('/', headers={'Origin': origin}) for origin in invalid_origins]
+
+    # ASSERT
+    for response, origin in zip(valid_responses, valid_origins):
+        assert response.headers.get('access-control-allow-origin') == origin
+
+    for response in invalid_responses:
+        assert response.headers.get('access-control-allow-origin') is None
