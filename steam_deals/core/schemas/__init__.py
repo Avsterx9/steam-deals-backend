5b525359f1f2879b51b34d418fb2a64533008ff7
Mateusz Żebrak <mateusz.zebrak@gmail.com>
2022-04-17 16:55:28 +0200

    Move Index schema to schemas/__init__.py module

--- steam_deals/core/schemas/__init__.py
+++ steam_deals/core/schemas/__init__.py
@@ -1,4 +1,14 @@
-from steam_deals.core.schemas.index import *
+from steam_deals.config import settings, VERSION
 from steam_deals.core.schemas.other import *
 from steam_deals.core.schemas.token import *
 from steam_deals.core.schemas.user import *
+
+
+class Index(BaseModel):
+    title: str = settings.PROJECT_TITLE
+    description: str = settings.PROJECT_DESC
+    message: str = 'For documentation please refer to /docs endpoint'
+    version: str = VERSION
+
+    def __repr__(self):
+        return str(self.__dict__)
