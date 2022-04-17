baabcc7be0d996643d9f61ee46005a0bd134d7b5
Mateusz Żebrak <mateusz.zebrak@gmail.com>
2022-04-17 16:51:23 +0200

    Rename schema utils.py -> other.py

--- steam_deals/core/schemas/__init__.py
+++ steam_deals/core/schemas/__init__.py
@@ -1,4 +1,4 @@
 from steam_deals.core.schemas.index import *
+from steam_deals.core.schemas.other import *
 from steam_deals.core.schemas.token import *
 from steam_deals.core.schemas.user import *
-from steam_deals.core.schemas.utils import *
